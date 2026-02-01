4dc52d0436066014ea642e73e19afc229f3b5a63
itview_demo <hasnainkhan57899@gmail.com>
2026-02-01 10:36:19 +0530

    fierst commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      
+      
+
+      <h1 className='text-blue-600 dark:text-sky-400 border-4 rounded-xl font-bold'>Design of our Fierst Web page Using Vite + React</h1>
+      
+    
+
+    </>
+  )
+}
+
+export default App
